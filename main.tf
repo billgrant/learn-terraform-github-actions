cddca8b2e07cfa3bcec9cfb5dd8d83425f1715a5
billgrant <bill.grant@hashicorp.com>
2023-05-23 10:25:39 -0400

    Moving to S3

--- main.tf
+++ main.tf
@@ -1,74 +1,122 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.52.0"
+      version = "~> 4.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.4.3"
+      version = "~> 3.4"
     }
   }
-  required_version = ">= 1.1.0"
 }
 
 provider "aws" {
-  region = "us-west-2"
-}
+  region = var.region
 
-resource "random_pet" "sg" {}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello World" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
-}
-
-resource "aws_security_group" "web-sg" {
-  name = "${random_pet.sg.id}-sg"
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  // connectivity to ubuntu mirrors is required to run `apt-get update` and `apt-get install apache2`
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  default_tags {
+    tags = {
+      environment = var.env
+      department  = "SEA"
+      application = "HashiCafe website"
+    }
   }
 }
 
-output "web-address" {
-  value = "${aws_instance.web.public_dns}:8080"
+locals {
+  timestamp = timestamp()
+}
+
+resource "random_integer" "product" {
+  min = 0
+  max = length(var.hashi_products) - 1
+  keepers = {
+    "timestamp" = local.timestamp
+  }
+}
+
+resource "aws_s3_bucket" "www_bucket" {
+  bucket_prefix = "${var.prefix}-hashicafe-website-${lower(var.env)}-"
+  force_destroy = true
+  tags = {
+    hc-internet-facing = "true"
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "www_bucket" {
+  bucket = aws_s3_bucket.www_bucket.id
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "www_bucket" {
+  bucket = aws_s3_bucket.www_bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "www_bucket" {
+  bucket                  = aws_s3_bucket.www_bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "www_bucket" {
+  bucket = aws_s3_bucket.www_bucket.id
+  policy = data.aws_iam_policy_document.s3_public_access_policy.json
+
+  depends_on = [aws_s3_bucket_public_access_block.www_bucket]
+}
+
+data "aws_iam_policy_document" "s3_public_access_policy" {
+  statement {
+    sid     = "PublicAccess"
+    effect  = "Allow"
+    actions = ["s3:GetObject"]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    resources = ["${aws_s3_bucket.www_bucket.arn}/*"]
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "www_bucket" {
+  bucket = aws_s3_bucket.www_bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "www_bucket" {
+  bucket = aws_s3_bucket.www_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_object" "index" {
+  key    = "index.html"
+  bucket = aws_s3_bucket.www_bucket.id
+  content = templatefile("files/index.html", {
+    product_name  = var.hashi_products[random_integer.product.result].name
+    product_color = var.hashi_products[random_integer.product.result].color
+    product_image = var.hashi_products[random_integer.product.result].image_file
+  })
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "images" {
+  for_each     = fileset("files/img/", "*.png")
+  bucket       = aws_s3_bucket.www_bucket.id
+  key          = "img/${each.value}"
+  source       = "files/img/${each.value}"
+  content_type = "image/png"
 }
